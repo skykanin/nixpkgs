2cb27e7b3a8b0a832ff44140e1f64808e02dde68
Mario Rodas <marsam@users.noreply.github.com>
2019-05-24 20:02:00 -0500

    pythonPackages.pytest: 4.2.1 -> 4.6.3

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1684,9 +1684,9 @@ in {
 
   pyhepmc = callPackage ../development/python-modules/pyhepmc { };
 
-  pytest = self.pytest_42;
+  pytest = self.pytest_4;
 
-  pytest_42 = callPackage ../development/python-modules/pytest {
+  pytest_4 = callPackage ../development/python-modules/pytest {
     # hypothesis tests require pytest that causes dependency cycle
     hypothesis = self.hypothesis.override { doCheck = false; };
   };
